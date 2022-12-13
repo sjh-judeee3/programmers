f60ac639095b0e656dffab4c936b159bd42cea7e
judys0303 <judys0303@gmail.com>
2022-12-14 00:03:30 +0900

    Update 삼총사.py

--- level-1/삼총사.py
+++ level-1/삼총사.py
@@ -5,7 +5,7 @@ def solution(number):
     for i in range(len(number)):
         for j in range(len(number)):
             for k in range(len(number)):
-                if i != j and j != k and k != i and (number[i] + number[j] + number[k] == 0):
+                if i != j and j != k and k != i and number[i] + number[j] + number[k] == 0:
                     total = total + 1
     
     answer = total/6
